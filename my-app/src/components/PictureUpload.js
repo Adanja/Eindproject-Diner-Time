c3a94180ddfe60c3e998dc249ff00063bcf4c20d
Adanja <adanja-s@hotmail.com>
2021-10-07 15:35:26 +0200

    Connecting recipy and picture form to the uploadRecipePage

--- my-app/src/components/PictureUpload.js
+++ my-app/src/components/PictureUpload.js
@@ -1,44 +1,44 @@
-import React, {useState, useContext} from 'react';
-import axios from 'axios';
-import { AuthContext } from '../context/AuthContextProvider';
-
-function PictureUpload(){
-    const [selectedFile, setSelectedFile] = useState();
-    const [registerSuccess, toggleRegisterSuccess] = useState(false);
-    const {login,logout, user,status} =  useContext(AuthContext);
-    const fileSelectionHandler = (event) => {
-        setSelectedFile(event.target.files[0]);
-        console.log('line 9 event.target.files',event.target.files)
-    }
-    const uploadFileHandler = async () => {
-        try {
-            let file = selectedFile
-            console.log('line 16 file',file)
-            let formData = new FormData()
-            formData.append('email', user.email)
-            formData.append('username',user.username)
-            // formData.append('password', '1234')
-            formData.append('file', file)
-            const config={headers: {'Content-Type': 'multipart/form-data'},}
-            const res = await axios.post('http://localhost:8080/file-upload', formData,config);
-            //const res = await axios.post('http://localhost:8080/users', formData);
-            console.log(res.data);
-        } catch (err) {
-            console.log('line 23 error')
-            console.error(err);
-        }
-    };
-
-    return(
-        <form onSubmit={uploadFileHandler}>
-            <div className="">
-                <label>
-                    Select File
-                </label>
-                <input type="file" name="file" onChange={fileSelectionHandler}/>
-                <button type='submit'>Upload</button>
-            </div>
-        </form>
-    )
-}
-export default PictureUpload;
+// import React, {useState, useContext} from 'react';
+// import axios from 'axios';
+// import { AuthContext } from '../context/AuthContextProvider';
+//
+// function PictureUpload(){
+//     const [selectedFile, setSelectedFile] = useState();
+//     const [registerSuccess, toggleRegisterSuccess] = useState(false);
+//     const {login,logout, user,status} =  useContext(AuthContext);
+//     const fileSelectionHandler = (event) => {
+//         setSelectedFile(event.target.files[0]);
+//         console.log('line 9 event.target.files',event.target.files)
+//     }
+//     const uploadFileHandler = async () => {
+//         try {
+//             let file = selectedFile
+//             console.log('line 16 file',file)
+//             let formData = new FormData()
+//             formData.append('email', user.email)
+//             formData.append('username',user.username)
+//             // formData.append('password', '1234')
+//             formData.append('file', file)
+//             const config={headers: {'Content-Type': 'multipart/form-data'},}
+//             const res = await axios.post('http://localhost:8080/file-upload', formData,config);
+//             //const res = await axios.post('http://localhost:8080/users', formData);
+//             console.log(res.data);
+//         } catch (err) {
+//             console.log('line 23 error')
+//             console.error(err);
+//         }
+//     };
+//
+//     return(
+//         <form onSubmit={uploadFileHandler}>
+//             <div className="">
+//                 <label>
+//                     Select File
+//                 </label>
+//                 <input type="file" name="file" onChange={fileSelectionHandler}/>
+//                 <button type='submit'>Upload</button>
+//             </div>
+//         </form>
+//     )
+// }
+// export default PictureUpload;
--- my-app/src/pages/uploadpaginaRecepten.js
+++ my-app/src/pages/uploadpaginaRecepten.js
@@ -1,19 +1,30 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import "./uploadpaginaRecepten.css";
-import PictureUpload from "../components/PictureUpload";
 import axios from "axios";
-import {AuthContext} from "../context/AuthContextProvider";
+// import {AuthContext} from "../context/AuthContextProvider";
+import {useHistory} from "react-router-dom";
+import {useForm} from "react-hook-form";
 import {forkJoin, map} from "rxjs";
 
-function UploadpaginaRecepten(props) {
+
+
+function UploadpaginaRecepten() {
 
     const [selectedFile, setSelectedFile] = useState();
-    const [registerSuccess, toggleRegisterSuccess] = useState(false);
-    const {login,logout, user,status} =  useContext(AuthContext);
+    // const [registerSuccess, toggleRegisterSuccess] = useState(false);
+    // const {login,logout, user,status} =  useContext(AuthContext);
     const fileSelectionHandler = (event) => {
         setSelectedFile(event.target.files[0]);
         console.log('line 9 event.target.files',event.target.files)
     }
+
+    const { handleSubmit, formState: { errors }, register, control} = useForm();
+    const message = "Dit veld mag niet leeg blijven"
+    const history = useHistory();
+    const token = localStorage.getItem("token");
+    const [file, setFile] = useState({});
+    const [url, setUrl] = useState({});
+
     const uploadFileHandler = async () => {
         try {
             let file = selectedFile
@@ -33,18 +44,15 @@ function UploadpaginaRecepten(props) {
         }
     };
 
-    // const token = localStorage
-    //
-    //
-    //
-    // async function onSubmit(recipe) {
-    //     forkJoin([
-    //         uploadRecipies(recipe),
-    //         uploadPicture()
-    //     ]).pipe(map(([machine, picture]) => {
-    //            assignPictureToMachine(machine.data.id, picture.data.message)
-    //        })).subscribe();    //
-    // };
+
+    async function onSubmit(recipe) {
+        forkJoin([
+            uploadRecipies(recipe),
+            uploadPicture()
+        ]).pipe(map(([machine, picture]) => {
+               assignPictureToMachine(machine.data.id, picture.data.message)
+           })).subscribe();    //
+    };
 
     function uploadRecipe(recipe) {
         return axios.post("http://localhost:8080/recipies",
@@ -54,7 +62,7 @@ function UploadpaginaRecepten(props) {
                     Authorization: `Bearer ${token}`,
                 },
                 name: recipe.recipe.name,
-                difficultyRating: recipe_difficulty_rating,
+                difficultyRating: recipe.recipe_difficulty_rating,
                 description: recipe.recipe_description,
                 time: recipe.recipe_time,
 
@@ -62,7 +70,7 @@ function UploadpaginaRecepten(props) {
     }
 
     function uploadPicture() {
-        let FormDaata = new FormData();
+        let formData = new FormData();
         formData.append("file", file);
         return axios.post("http://localhost:8080/pictures/upload", formData,
             {
@@ -76,7 +84,7 @@ function UploadpaginaRecepten(props) {
 
     async function assignPictureToRecipe(recipeId, pictureId) {
         try {
-            const result = await axios.put(`http://localhost:8080/recipies/recipy/${recipeId}/picture`,
+            const result = await axios.put(`http://localhost:8080/recipies/recipe/${recipeId}/pictures`,
                 {
                     headers: {
                         "Content-Type": "application/json",
@@ -93,13 +101,23 @@ function UploadpaginaRecepten(props) {
     return (
         <div>
             <h1>Recept upload pagina</h1>
-            <form action="">
+            <form
+                onSubmit={handleSubmit(onsubmit)}
+                encType={"multipart/form-data"}
+            >
+
                 <div className={"left-form"}>
                     <h5>Recept titel</h5>
-                    <input/>
+                    <input
+                        id={"recipe_name"}
+                        placeholder={"Recept titel"}
+                        {...register("recipe_name", {required: { value: true, message:message}})}
+                    />{errors.recipe_name && <p>{errors.recipe_name/message}</p>}
 
                     <h5>Beschrijving</h5>
-                    <textarea className={"form-beschrijving"} placeholder={"Typ hier je beschrijving.."}>
+                    <textarea className={"form-beschrijving"}
+                              id={"recipe_description"}
+                              placeholder={"Typ hier je beschrijving.."}>
                     </textarea>
 
                     <h5>Ingrediëntenlijst</h5>
@@ -123,7 +141,7 @@ function UploadpaginaRecepten(props) {
                     <input type={"file"} className={"file-upload"}/>
 
                     <h5>Bereidingstijd</h5>
-                    <select>
+                    <select id={"recipe_time"} required={true}>
                         <option value="20">20 minuten</option>
                         <option value="30">30 minuten</option>
                         <option selected value="40">40 minuten</option>
@@ -132,7 +150,7 @@ function UploadpaginaRecepten(props) {
                     </select>
 
                     <h5>Moeilijkheidsgraad</h5>
-                    <select>
+                    <select id={"recipe_difficulty_rating"}>
                         <option value="EASY">Makkelijk</option>
                         <option value="INTERMEDIATE">Gemiddeld</option>
                         <option selected value="HARD">Moeilijk</option>
